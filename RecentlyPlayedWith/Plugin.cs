6a33c5ee21804b5c9370ef7e5f0a01ab23348a12
1A3 <mcplayer1a3@gmail.com>
2024-01-01 16:30:46 +0000

    corrected target framework to 2.1

--- RecentlyPlayedWith/Plugin.cs
+++ RecentlyPlayedWith/Plugin.cs
@@ -7,102 +7,103 @@ using GameNetcodeStuff;
 using HarmonyLib;
 using Steamworks;
 
-namespace LobbyInviteOnly;
-
-[BepInPlugin(modGUID, "RecentlyPlayedWith", modVersion)]
-internal class PluginLoader : BaseUnityPlugin
+namespace LobbyInviteOnly
 {
-    internal const string modGUID = "Dev1A3.RecentlyPlayedWith";
-    internal const string modVersion = "1.0.0";
-
-    private readonly Harmony harmony = new Harmony(modGUID);
-
-    private static bool initialized;
-
-    public static PluginLoader Instance { get; private set; }
-
-    private void Awake()
+    [BepInPlugin(modGUID, "RecentlyPlayedWith", modVersion)]
+    internal class PluginLoader : BaseUnityPlugin
     {
-        if (initialized)
+        internal const string modGUID = "Dev1A3.RecentlyPlayedWith";
+        internal const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static bool initialized;
+
+        public static PluginLoader Instance { get; private set; }
+
+        private void Awake()
         {
-            return;
-        }
-        initialized = true;
-        Instance = this;
-        Assembly patches = Assembly.GetExecutingAssembly();
-        harmony.PatchAll(patches);
-
-        PlayedWithConfig.InitConfig();
-    }
-
-    public void BindConfig<T>(ref ConfigEntry<T> config, string section, string key, T defaultValue, string description = "")
-    {
-        config = Config.Bind<T>(section, key, defaultValue, description);
-    }
-}
-internal class PlayedWithConfig
-{
-    internal static SteamId LobbyId;
-
-    internal static ManualLogSource logSource;
-
-    internal static ConfigEntry<bool> IncludeOrbit;
-    internal static void InitConfig()
-    {
-        logSource = Logger.CreateLogSource(PluginLoader.modGUID);
-
-        PluginLoader.Instance.BindConfig(ref IncludeOrbit, "Settings", "Enable In Orbit", false, "Should players be classed as recently played with whilst you are in orbit? Disabling this will only add people whilst the ship is landed.");
-    }
-
-    internal static HashSet<ulong> PlayerList = new HashSet<ulong>();
-    internal static void SetPlayedWith(ulong playerSteamId, string debugType)
-    {
-        if (!PlayerList.Contains(playerSteamId) && playerSteamId != 0f && playerSteamId != StartOfRound.Instance.localPlayerController.playerSteamId)
-        {
-            PlayerList.Add(playerSteamId);
-            SteamFriends.SetPlayedWith(playerSteamId);
-            logSource.LogInfo($"Set recently played with {playerSteamId} ({debugType})");
-        }
-    }
-}
-
-[HarmonyPatch]
-internal static class SetPlayedWith_Patch
-{
-    [HarmonyPatch(typeof(StartOfRound), "openingDoorsSequence")]
-    [HarmonyPostfix]
-    private static void openingDoorsSequence(ref StartOfRound __instance)
-    {
-        foreach (PlayerControllerB plyCon in __instance.allPlayerScripts)
-        {
-            if (plyCon.isPlayerControlled)
+            if (initialized)
             {
-                PlayedWithConfig.SetPlayedWith(plyCon.playerSteamId, "shipLanded");
+                return;
+            }
+            initialized = true;
+            Instance = this;
+            Assembly patches = Assembly.GetExecutingAssembly();
+            harmony.PatchAll(patches);
+
+            PlayedWithConfig.InitConfig();
+        }
+
+        public void BindConfig<T>(ref ConfigEntry<T> config, string section, string key, T defaultValue, string description = "")
+        {
+            config = Config.Bind<T>(section, key, defaultValue, description);
+        }
+    }
+    internal class PlayedWithConfig
+    {
+        internal static SteamId LobbyId;
+
+        internal static ManualLogSource logSource;
+
+        internal static ConfigEntry<bool> IncludeOrbit;
+        internal static void InitConfig()
+        {
+            logSource = Logger.CreateLogSource(PluginLoader.modGUID);
+
+            PluginLoader.Instance.BindConfig(ref IncludeOrbit, "Settings", "Enable In Orbit", false, "Should players be classed as recently played with whilst you are in orbit? Disabling this will only add people whilst the ship is landed.");
+        }
+
+        internal static HashSet<ulong> PlayerList = new HashSet<ulong>();
+        internal static void SetPlayedWith(ulong playerSteamId, string debugType)
+        {
+            if (!PlayerList.Contains(playerSteamId) && playerSteamId != 0f && playerSteamId != StartOfRound.Instance.localPlayerController.playerSteamId)
+            {
+                PlayerList.Add(playerSteamId);
+                SteamFriends.SetPlayedWith(playerSteamId);
+                logSource.LogInfo($"Set recently played with {playerSteamId} ({debugType})");
             }
         }
     }
 
-    [HarmonyPatch(typeof(PlayerControllerB), "SendNewPlayerValuesClientRpc")]
-    [HarmonyPostfix]
-    private static void StartClient(ref ulong[] playerSteamIds)
+    [HarmonyPatch]
+    internal static class SetPlayedWith_Patch
     {
-        if (StartOfRound.Instance != null && (StartOfRound.Instance.shipHasLanded || (StartOfRound.Instance.inShipPhase && PlayedWithConfig.IncludeOrbit.Value)))
+        [HarmonyPatch(typeof(StartOfRound), "openingDoorsSequence")]
+        [HarmonyPostfix]
+        private static void openingDoorsSequence(ref StartOfRound __instance)
         {
-            string debugType = "otherJoined";
-            if (GameNetworkManager.Instance.currentLobby.HasValue && GameNetworkManager.Instance.currentLobby.Value.Id != PlayedWithConfig.LobbyId)
+            foreach (PlayerControllerB plyCon in __instance.allPlayerScripts)
             {
-                PlayedWithConfig.LobbyId = GameNetworkManager.Instance.currentLobby.Value.Id;
-                debugType = "selfJoined";
-                if (PlayedWithConfig.PlayerList.Count > 0)
+                if (plyCon.isPlayerControlled)
                 {
-                    PlayedWithConfig.PlayerList.Clear();
-                    PlayedWithConfig.logSource.LogInfo($"Cleared recently played with...");
+                    PlayedWithConfig.SetPlayedWith(plyCon.playerSteamId, "shipLanded");
                 }
             }
+        }
 
-            foreach (ulong steamId in playerSteamIds)
+        [HarmonyPatch(typeof(PlayerControllerB), "SendNewPlayerValuesClientRpc")]
+        [HarmonyPostfix]
+        private static void StartClient(ref ulong[] playerSteamIds)
+        {
+            if (StartOfRound.Instance != null && (StartOfRound.Instance.shipHasLanded || (StartOfRound.Instance.inShipPhase && PlayedWithConfig.IncludeOrbit.Value)))
             {
-                PlayedWithConfig.SetPlayedWith(steamId, debugType);
+                string debugType = "otherJoined";
+                if (GameNetworkManager.Instance.currentLobby.HasValue && GameNetworkManager.Instance.currentLobby.Value.Id != PlayedWithConfig.LobbyId)
+                {
+                    PlayedWithConfig.LobbyId = GameNetworkManager.Instance.currentLobby.Value.Id;
+                    debugType = "selfJoined";
+                    if (PlayedWithConfig.PlayerList.Count > 0)
+                    {
+                        PlayedWithConfig.PlayerList.Clear();
+                        PlayedWithConfig.logSource.LogInfo($"Cleared recently played with...");
+                    }
+                }
+
+                foreach (ulong steamId in playerSteamIds)
+                {
+                    PlayedWithConfig.SetPlayedWith(steamId, debugType);
+                }
             }
         }
     }
